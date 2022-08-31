6ac4a85ceb1c1f55b62ba451503eb6a9d3fbf668
catacola199 <fufideira17@gmail.com>
2022-08-31 18:51:12 +0700

    12
    12

--- db_sip.sql
+++ db_sip.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Okt 2021 pada 10.50
--- Versi server: 10.5.11-MariaDB
--- Versi PHP: 7.4.21
+-- Generation Time: Jul 19, 2022 at 07:41 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,15 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `absensi_online`
+-- Database: `db_sip`
 --
-CREATE DATABASE IF NOT EXISTS `absensi_online` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `absensi_online`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `db_absensi`
+-- Table structure for table `db_absensi`
 --
 
 CREATE TABLE `db_absensi` (
@@ -45,7 +43,7 @@ CREATE TABLE `db_absensi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `db_rememberme`
+-- Table structure for table `db_rememberme`
 --
 
 CREATE TABLE `db_rememberme` (
@@ -64,7 +62,7 @@ CREATE TABLE `db_rememberme` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `db_setting`
+-- Table structure for table `db_setting`
 --
 
 CREATE TABLE `db_setting` (
@@ -81,16 +79,16 @@ CREATE TABLE `db_setting` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `db_setting`
+-- Dumping data for table `db_setting`
 --
 
 INSERT INTO `db_setting` (`status_setting`, `nama_instansi`, `jumbotron_lead_set`, `nama_app_absensi`, `logo_instansi`, `timezone`, `absen_mulai`, `absen_mulai_to`, `absen_pulang`, `maps_use`) VALUES
-(1, '[Ubah Nama Instansi]', '[Ubah Text Berjalan Halaman Depan Disini Pada Setting Aplikasi]', 'Absensi Online', 'default-logo.png', 'Asia/Jakarta', '06:00:00', '11:00:00', '16:00:00', 1);
+(1, 'Sri Intan Perkasa', 'Selamat Datang di Halaman Administrator Sri Intan Perkasa', 'Sri Intan Perkasa', '719a40671a364cc8f0b006e59e72651a.png', 'Asia/Jakarta', '06:00:00', '11:00:00', '16:00:00', 1);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -117,55 +115,56 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id_pegawai`, `nama_lengkap`, `username`, `password`, `role_id`, `umur`, `image`, `qr_code_image`, `kode_pegawai`, `instansi`, `jabatan`, `npwp`, `tgl_lahir`, `tempat_lahir`, `jenis_kelamin`, `bagian_shift`, `is_active`, `qr_code_use`, `last_login`, `date_created`) VALUES
-(12, 'Admin', 'admin', '$2y$10$sZVyS3G6aVjMRoLq0JhuZuiAat.QjOOtbcyohRih3IxtQaEvJG4Eq', 1, 18, 'default.png', 'no-qrcode.png', '293571010111', '[Ubah Nama Instansi]', 'Test', 'Tidak Ada', '2020-09-08', 'Test', 'Laki - Laki', 1, 1, 0, 1634813295, 1584698797);
+(12, 'Admin', 'admin', '$2y$10$YASEM25LIhUKkAsZrby/MOaZ1gDYzpPmRb5muDwLU5/R9/s98aPHe', 1, 18, '9afc2148d283471279c5952cce760888.png', 'no-qrcode.png', '293571010111', 'Sri Intan Perkasa', 'Test', 'Tidak Ada', '2020-09-08', 'Test', 'Laki - Laki', 1, 1, 0, 1657532813, 1584698797),
+(42, 'my', 'my', '$2y$10$.094byLiu9FKsmYLPwWEoeNVqP2jYbtrmR3a/Zz/mq0pMksDorZoq', 2, 23, 'f3d9c01d57ffd21eb2dd110d46a980db.png', 'no-qrcode.png', '441790685330825', 'Sri Intan Perkasa', 'CEO', 'Tidak Ada', '1999-11-11', 'Batang', 'Laki - Laki', 1, 1, 0, 1657198860, 1657198048);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `db_absensi`
+-- Indexes for table `db_absensi`
 --
 ALTER TABLE `db_absensi`
   ADD PRIMARY KEY (`id_absen`);
 
 --
--- Indeks untuk tabel `db_rememberme`
+-- Indexes for table `db_rememberme`
 --
 ALTER TABLE `db_rememberme`
   ADD PRIMARY KEY (`id_session`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `db_absensi`
+-- AUTO_INCREMENT for table `db_absensi`
 --
 ALTER TABLE `db_absensi`
-  MODIFY `id_absen` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_absen` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `db_rememberme`
+-- AUTO_INCREMENT for table `db_rememberme`
 --
 ALTER TABLE `db_rememberme`
-  MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_session` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
